05dcb1bd1c04b9372fed8cd673ed65712ab8821f
Jared Leach <leach.jared@gmail.com>
2019-09-24 11:50:49 -0700

    Initial commit

---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+// What is a genetic algorithm
+// Artificial Intelligience algorithm inspired by evolution
+// Population
+	// Array of individuals
+// Individual (Represented by genes)
+	// Array of genes
+// Natural Selecetion
+	// Fitness function that calculates fit
+// Reproduction
+	// Crossover
+// Mutation
+	// Mutation function
+public class DNA<T>
+{
+	public T[] Genes{get; private set;}
+	public float Fitness{get; private set;}
+	private Random random;
+
+	public DNA(int size, Random random)
+	{
+		Genes = new T[size];
+		this.random = random;
+	}
+	public float CalculateFitness()
+	{
+		return 0;
+	}
+	public DNA<T> Crossover(DNA<T> otherParent)
+	{
+		// We know that the output of the crossover function will be a child as this function is simulating reproduction
+		DNA<T> child = new DNA<T>(Genes.Length, this.random);
+		// Create a random that will be used to determine which parents genes will be used
+		Random random = new Random();
+		for (int i = 0; i < Genes.Length; i++)
+		{
+			
+			child.Genes[i] = random.NextDouble() < 0.5f ? Genes[i] : otherParent.Genes[i];
+		}
+		return child;
+	}
+	public void Mutate(float mutationRate)
+	{
+		for (int i = 0; i < Genes.Length; i++)
+		{
+			if(random.NextDouble() < mutationRate)
+			{
+				// Genes[i] = ???
+			}
+		}
+	}
+
+}
